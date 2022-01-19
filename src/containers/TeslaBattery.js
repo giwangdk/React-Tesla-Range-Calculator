d9f3471c874ef9d8cfce57dee50fd927f55e68bf
gidwikintan <gidwikintan@gmail.com>
2022-01-19 21:06:59 +0700

    create components tesla battery

---
+++ src/containers/TeslaBattery.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TeslaBattery extends Component {
+  render() {
+      return (
+        <form className='tesla-battery'>
+            <h1>Range Per Charge</h1>
+        </form>
+    )
+  }
+}
