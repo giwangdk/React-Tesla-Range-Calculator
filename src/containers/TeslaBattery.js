f30243534e37e996077a81ee258bb5eb758d81c5
gidwikintan <gidwikintan@gmail.com>
2022-01-24 05:29:15 +0700

    add wheels and style it and add animation

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import TeslaCar from '../components/TeslaCar/TeslaCar';
 import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
 
 export default class TeslaBattery extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      carstats: [],
+      config: {
+        speed: 55,
+        temperature: 20,
+        climate: true,
+        wheels:19
+      }
+    }
+  }
   render() {
+
+    const {config} = this.state
       return (
         <form className='tesla-battery'>
             <h1>Range Per Charge</h1>
+          <TeslaCar wheelsize={config.wheels}/>
             <TeslaNotice/>
         </form>
     )
